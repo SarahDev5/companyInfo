42ad7dcf28e9a8e9253af177a99c90cf4ced9a69
SarahDev5 <sarahrossitter1234gmail.com>
2025-11-04 11:01:06 +0000

    initial files

---
+++ script.js
@@ -0,0 +1,19 @@
+const myCompany = {
+ name: "Digital Solutions UK",
+ cultureType: "clan",
+ britishValues: [
+ "mutual respect",
+ "individual liberty",
+ "democracy"
+ ],
+ foundedYear: 2019,
+ employeeCount: 35,
+ 
+ jobs: [
+ {
+ title: "Interaction Engineer",
+ description: "Join our supportive team...",
+ skills: ["HTML", "CSS", "JavaScript"]
+ }
+ ]
+};
