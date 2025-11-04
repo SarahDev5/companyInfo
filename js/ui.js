60eaf56a4818a138fd9eca93f8d18dc8ca42c578
francis-faolan <francis.faolan@gmail.com>
2025-11-04 12:45:26 +0000

    put structure in place for me to fill in on the ui page

---
+++ js/ui.js
@@ -0,0 +1,21 @@
+//confirming data available
+console.log("UI file connected");
+console.log("Company Data check:", myCompany);
+
+//find html elements (references to placeholders)
+//e.g. 
+const nameEl = document.getElementById("companyName");
+
+
+//insert company data (static info from myCompany)
+//e.g.
+nameEl.textContent = myCompany.name;
+
+//Display British Values (loop through the array and list items)
+
+
+//Display jobs (loop through the jobs to show job cards)
+
+//need the const, the job card innet html, and the container
+
+//if more time, buttons
